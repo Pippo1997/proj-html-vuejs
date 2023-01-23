08228a278ee8a93c56c32c32e57ed5540c1be812
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-01-23 18:57:34 +0100

    aggiunta navbar reattiva + content bottom header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,30 @@
 <script>
+import AppNavbar from './AppNavbar.vue'
 export default {
-  
+  components: {
+    AppNavbar,
+  },
+  data() {
+    return{
+      Menu:[
+        {
+          label: 'Home',
+        },
+        {
+          label: 'Features',
+        },
+        {
+          label: 'Achivment',
+        },
+        {
+          label: 'Blog',
+        },
+        {
+          label: 'Contant',
+        },
+      ]
+    }
+  }
 }
 </script>
 
@@ -39,7 +63,22 @@ export default {
         </div>
       </div>
       <div class="header-bottom">
-
+        <div class="d-flex flex-wrap align-items-center justify-content-between">
+          <div class="logo">
+            <img src="/imgs/logo.png" alt="logo">
+          </div>
+          <div class="menu d-flex">
+            <AppNavbar :Menu="Menu"/>
+            <a href="/" class="login">
+              <i class="fa-solid fa-user"></i>
+              <span>LOG IN</span>
+            </a>
+            <a href="/" class="sing-up">
+              <i class="fa-solid fa-users"></i>
+              <span>SING IN</span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -56,6 +95,16 @@ export default {
 
     li{
       padding: 15px 25px;
+
+      &::after{
+        content: "";
+        height: 120%;
+        width: 1px;
+        background-color: rgba(255, 255, 255, 0.1);
+        left: -20px;
+        top: -7px;
+        transform: rotate(-25deg);
+      }
     }
   }
 
@@ -64,8 +113,66 @@ export default {
 
     li{
       padding: 15px 25px;
+
+      &::after{
+        content: "";
+        height: 120%;
+        width: 1px;
+        background-color: rgba(255, 255, 255, 0.1);
+        right: 0;
+        top: -5px;
+        transform: rotate(25deg);
+      }
     }
   } 
+}
+
+.header-bottom{
+
+  .menu{
+    margin-right: 30px;
+
+    .login{
+      padding: 15px;
+      color: #ff0052;
+      border-radius: 2px 0 0 2px;
+
+      &::before{
+        content: "";
+        bottom: 0;
+        left: 1px;
+        height: 2px;
+        width: 100%;
+        background: #ff0052;
+      }
+    }
+
+    .sing-up{
+      padding: 15px;
+      color: white;
+      border-radius: 0 2px 2px 0;
+      background: #ff0052;
+
+      &::before{
+        content: "";
+        bottom: 0;
+        left: 1px;
+        height: 2px;
+        width: 100%;
+        background:white;
+        border-radius: 2px;
+      }
+
+      &::after{
+        content: "";
+        bottom: 0;
+        right: -1px;
+        width: 2px;
+        height: calc(100% - 2px);
+        background:white;
+      }
+    }
+  }
 }
 
 </style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    Menu: Array
+  }
+}
+</script>
+
+<template lang="">
+  <nav class="d-flex flex-wrap align-items-center justify-content-between">
+    <ul class="d-flex">
+        <li v-for="(item, index) in Menu" :key="index">
+            <a href="/">{{ item.label }}</a>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss">
+
+    nav{
+        margin-right: 30px;
+
+        li{
+            color: #fff;
+            font-size: 1rem;
+            font-weight: 700;
+            padding: 15px 22px;
+            text-transform: uppercase;
+        }
+    }
+</style>
